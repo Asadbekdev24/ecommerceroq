9eb7734d989c288d18df928d909f54bca646c9ea
amirovsunnat <sunnatbekamirov22@gmail.com>
2025-01-23 10:57:06 +0500

    Custom Scroll View instead of the view we used.

--- lib/presentation/screens/products_screen.dart
+++ lib/presentation/screens/products_screen.dart
@@ -37,21 +37,6 @@ class _ProductsScreenState extends State<ProductsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          onPressed: () async {
-            await showSearch(
-              context: context,
-              delegate: SearchListDelegate(
-                products: context.read<ProductProvider>().products,
-              ),
-            );
-          },
-          icon: Icon(
-            Icons.search,
-          ),
-        ),
-      ),
       body: Consumer<ProductProvider>(
         builder: (context, productProvider, child) {
           if (productProvider.isLoading) {
@@ -64,41 +49,129 @@ class _ProductsScreenState extends State<ProductsScreen> {
               child: Text("No products found"),
             );
           }
-          return GridView.builder(
-            controller: scrollController,
-            itemCount: productProvider.isLoadingMore
-                ? productProvider.products.length + 1
-                : productProvider.products.length,
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 1,
-            ),
-            itemBuilder: (context, index) {
-              if (index >= productProvider.products.length) {
-                return const Padding(
-                  padding: EdgeInsets.symmetric(vertical: 20),
-                  child: Center(
-                    child: CircularProgressIndicator(),
-                  ),
-                );
-              }
-              final ProductModel product = productProvider.products[index];
-              return ProductCard(
-                onEdit: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => CreateProductScreen(
-                        productModel: product,
-                      ),
+          // return CustomScrollView(
+          //   physics: BouncingScrollPhysics(),
+          //   slivers: [
+          //     SliverAppBar(
+          //       collapsedHeight: 70,
+          //       stretch: true,
+          //       centerTitle: true,
+          //       // pinned: true,
+          //       // floating: true,
+          //       // snap: true,
+
+          //       expandedHeight: 300,
+          //       flexibleSpace: FlexibleSpaceBar(
+          //         stretchModes: [
+          //           StretchMode.zoomBackground,
+          //         ],
+          //         expandedTitleScale: 1.9,
+          //         title: Text("Products"),
+          //         background: Image.network(
+          //           fit: BoxFit.cover,
+          //           "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg",
+          //         ),
+          //       ),
+          //       scrolledUnderElevation: 0,
+          //       backgroundColor: Colors.pinkAccent,
+          //     ),
+          //     // SliverPadding(
+          //     //   padding: const EdgeInsets.all(16),
+          //     //   sliver: SliverGrid.builder(
+          //     //     itemCount: productProvider.products.length,
+          //     //     gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          //     //       crossAxisCount: 1,
+          //     //     ),
+          //     //     itemBuilder: (context, index) {
+          //     //       final ProductModel product =
+          //     //           productProvider.products[index];
+          //     //       return ProductCard(
+          //     //         onEdit: () {
+          //     //           Navigator.push(
+          //     //             context,
+          //     //             MaterialPageRoute(
+          //     //               builder: (context) => CreateProductScreen(
+          //     //                 productModel: product,
+          //     //               ),
+          //     //             ),
+          //     //           );
+          //     //         },
+          //     //         title: product.title.toString(),
+          //     //         image: product.images![0],
+          //     //         price: product.price!,
+          //     //         id: product.id!,
+          //     //       );
+          //     //     },
+          //     //   ),
+          //     // ),
+          //     SliverList.builder(
+          //       itemCount: productProvider.products.length,
+          //       itemBuilder: (context, index) {
+          //         final ProductModel product = productProvider.products[index];
+          //         return SizedBox(
+          //           height: 350,
+          //           child: ProductCard(
+          //             onEdit: () {
+          //               Navigator.push(
+          //                 context,
+          //                 MaterialPageRoute(
+          //                   builder: (context) => CreateProductScreen(
+          //                     productModel: product,
+          //                   ),
+          //                 ),
+          //               );
+          //             },
+          //             title: product.title.toString(),
+          //             image: product.images![0],
+          //             price: product.price!,
+          //             id: product.id!,
+          //           ),
+          //         );
+          //       },
+          //     ),
+          //     SliverToBoxAdapter(
+          //       child: Container(
+          //         child: Text(
+          //           "Container",
+          //         ),
+          //       ),
+          //     )
+          //   ],
+          // );
+          return NestedScrollView(
+            // The headerSliverBuilder callback defines the sliver widgets in the header
+            headerSliverBuilder:
+                (BuildContext context, bool innerBoxIsScrolled) {
+              return <Widget>[
+                // SliverAppBar is the header that remains visible while scrolling
+                SliverAppBar(
+                  expandedHeight: 200.0,
+                  floating: false,
+                  pinned: true,
+                  flexibleSpace: FlexibleSpaceBar(
+                    title: Text(
+                      'Nested Scroll View Example',
+                      style: TextStyle(
+                          fontSize: 16), // Customize the title's style
                     ),
-                  );
-                },
-                title: product.title.toString(),
-                image: product.images![0],
-                price: product.price!,
-                id: product.id!,
-              );
+                    background: Image.network(
+                      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSf9fG0XSXlw5HHtdVIhc1_gl4vzcKeCOAkoBD07BHiTAyvsVoKqvRbLkwuNSTheOd3Kk4&usqp=CAU',
+                      fit: BoxFit
+                          .cover, // Ensure the image covers the entire area
+                    ),
+                  ),
+                ),
+              ];
             },
+            // The body contains the scrollable content
+            body: ListView.builder(
+              itemCount: 20,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  title: Text('Item $index'), // Display a list of items
+                );
+              },
+            ),
           );
         },
       ),
